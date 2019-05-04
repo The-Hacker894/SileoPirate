7e3dfd8857e46300d1fa28db084d312d343f7dfc
The-Hacker894 <skylarmccauley894@gmail.com>
2019-05-04 18:25:41 -0400

    v0.2 - Beta

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+chmod 0755 xyz.skylarmccauley.sileopirate/DEBIAN
+chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/postinst
+chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/prerm
+
 find . -name ".DS_Store" -delete
 
 dpkg -b xyz.skylarmccauley.sileopirate
