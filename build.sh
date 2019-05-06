9ef0b5edbac1b34e224324b1bc6f68bf0a6028b5
The-Hacker894 <skylarmccauley894@gmail.com>
2019-05-06 16:47:45 -0400

    SileoPirate Helper

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ chmod 0755 xyz.skylarmccauley.sileopirate/DEBIAN
 chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/postinst
 chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/prerm
 
+chmod 0755 xyz.skylarmccauley.sileopirate.helper/DEBIAN
+chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/postinst
+chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/prerm
+
 find . -name ".DS_Store" -delete
 
 dpkg -b xyz.skylarmccauley.sileopirate
+dpkg -b xyz.skylarmccauley.sileopirate.helper
