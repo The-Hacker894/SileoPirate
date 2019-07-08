f048670fdb729fa38dd0fed0cb0cc75cf7ba28b6
TheHacker894 <skylarmccauley894@gmail.com>
2019-07-08 14:33:48 -0400

    Snapshot Build for v0.3
    Not recommended for use

--- build.sh
+++ build.sh
@@ -8,7 +8,15 @@ chmod 0755 xyz.skylarmccauley.sileopirate.helper/DEBIAN
 chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/postinst
 chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/prerm
 
+chmod 0755 xyz.skylarmccauley.sileopirate.theme/DEBIAN
+
 find . -name ".DS_Store" -delete
+
+mv default/*.strings.plist default/*.strings
+mv default/*.stringsdict.plist default/*.stringsdict
+mv pirate/*.strings.plist pirate/*.strings
+mv pirate/*.stringsdict.plist pirate/*.stringsdict
 
 dpkg -b xyz.skylarmccauley.sileopirate
 dpkg -b xyz.skylarmccauley.sileopirate.helper
+dpkg -b xyz.skylarmccauley.sileopirate.theme
