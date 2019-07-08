c37a241da034902b369bd4379770d6436e869ba2
The-Hacker894 <skylarmccauley894@gmail.com>
2019-07-08 18:21:09 -0400

    v0.3 Snapshot (Don't use)

--- build.sh
+++ build.sh
@@ -5,17 +5,20 @@ chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/postinst
 chmod 0555 xyz.skylarmccauley.sileopirate/DEBIAN/prerm
 
 chmod 0755 xyz.skylarmccauley.sileopirate.helper/DEBIAN
-chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/postinst
-chmod 0555 xyz.skylarmccauley.sileopirate.helper/DEBIAN/prerm
 
 chmod 0755 xyz.skylarmccauley.sileopirate.theme/DEBIAN
 
 find . -name ".DS_Store" -delete
 
-mv default/*.strings.plist default/*.strings
-mv default/*.stringsdict.plist default/*.stringsdict
-mv pirate/*.strings.plist pirate/*.strings
-mv pirate/*.stringsdict.plist pirate/*.stringsdict
+mv pirate/Categories.strings.plist pirate/Categories.strings
+mv pirate/Errors.strings.plist pirate/Errors.strings
+mv pirate/Localizable.strings.plist pirate/Localizable.strings
+mv pirate/Localizable.stringsdict.plist pirate/Localizable.stringsdict
+
+mv default/Categories.strings.plist default/Categories.strings
+mv default/Errors.strings.plist default/Errors.strings
+mv default/Localizable.strings.plist default/Localizable.strings
+mv default/Localizable.stringsdict.plist default/Localizable.stringsdict
 
 dpkg -b xyz.skylarmccauley.sileopirate
 dpkg -b xyz.skylarmccauley.sileopirate.helper
---
+++ revert.sh
@@ -0,0 +1,9 @@
+mv pirate/Categories.strings.plist pirate/Categories.strings.plist
+mv pirate/Errors.strings pirate/Errors.strings.plist
+mv pirate/Localizable.strings pirate/Localizable.strings.plist
+mv pirate/Localizable.stringsdict pirate/Localizable.stringsdict.plist
+
+mv default/Categories.strings default/Categories.strings.plist
+mv default/Errors.strings default/Errors.strings.plist
+mv default/Localizable.strings default/Localizable.strings.plist
+mv default/Localizable.stringsdict default/Localizable.stringsdict.plist
